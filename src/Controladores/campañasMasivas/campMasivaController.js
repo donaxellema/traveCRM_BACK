1fc0fca78de483fb5ea1923efb52e6238a785949
Alex <donaxellema@gmail.com>
2024-11-28 05:07:18 -0500

    Actualizacion de campañas masivas
    cambios en el controlador de campañas masivas

--- src/Controladores/campañasMasivas/campMasivaController.js
+++ src/Controladores/campañasMasivas/campMasivaController.js
@@ -13,42 +13,6 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer');
 
 
-// Configuración de la conexión a PostgreSQL
-
-
-// Controlador para manejar la carga y el procesamiento del archivo
-/* const uploadFile = async (req, res) => {
-  try {
-    const filePath = req.file.path;
-
-    // Leer el archivo Excel
-    const workbook = XLSX.readFile(filePath);
-    const sheetName = workbook.SheetNames[0];
-    const sheet = workbook.Sheets[sheetName];
-    const data = XLSX.utils.sheet_to_json(sheet);
-
-    // Iterar los datos y almacenarlos en PostgreSQL
-    for (const row of data) {
-      const { Nombre, Telefono } = row; // Ajusta estos nombres según tu Excel
-      if (Nombre && Telefono) {
-        await Pool.query(
-          `INSERT INTO crm_personas (pers_nombres, pers_telefono) VALUES ($1, $2)`,
-          [Nombre, Telefono]
-        );
-      }
-    }
-
-    // Eliminar el archivo subido
-    fs.unlinkSync(filePath);
-
-    res.status(200).json({ message: 'Datos cargados exitosamente.' });
-  } catch (error) {
-    console.error('Error procesando el archivo:', error);
-    res.status(500).json({ error: 'Error procesando el archivo.' });
-  }
-};
-
-module.exports = { uploadFile }; */
 
 
 
@@ -56,41 +20,8 @@ module.exports = { uploadFile }; */
 const upload = multer({ dest: 'uploads/' });
 
 exports.uploadContacts = async (req, res) => {
-  /*
-  try {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No se ha proporcionado un archivo.' });
-    }
 
-    const workbook = XLSX.readFile(req.file.path);
-    const sheetName = workbook.SheetNames[0];
-    const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
-    console.log("data")
-    console.log(data)
-
-    const query = `
-      INSERT INTO crm_whatsapp_contacts (contact_name, contact_etiqueta, contact_number)
-      VALUES ($1, $2, $3)
-      ON CONFLICT (contact_number) DO NOTHING
-    `;
-
-    for (const contact of data) {
-      const { name, etiqueta, number } = contact;
-      console.log( name, etiqueta, number )
-      await pool.query(query, [
-        name || 'Sin nombre',
-        etiqueta || 'Contacto de Excel',
-        number,
-      ]);
-    }
-
-    res.status(200).json({ message: 'Contactos importados exitosamente.', total: data.length });
-  } catch (error) {
-    console.error('Error al procesar archivo:', error);
-    res.status(500).json({ error: 'Error al procesar los contactos.' });
-  }*/
-
-
+  //implementacion del controlador para la implementacion de excel
     try {
       // Verificar si se cargó un archivo
       if (!req.file) {
